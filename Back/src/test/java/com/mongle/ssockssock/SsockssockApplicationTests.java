5a79366d9c34658663c0f84644fade156a178471
garam0107 <name3937@gmail.com>
2025-03-11 15:47:50 +0900

    S12P21E201-46 [BE] init: 스프링 프로젝트 초기 설정

---
+++ Back/src/test/java/com/mongle/ssockssock/SsockssockApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mongle.ssockssock;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SsockssockApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
