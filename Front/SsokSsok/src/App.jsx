e92f505eaf6fe873392cc5f351297dff9775520a
Yujiin <jiin4083@naver.com>
2025-03-17 15:03:26 +0900

    S12P21E201-56 [FE] feat: 메인화면 틀 잡기

--- Front/SsokSsok/src/App.jsx
+++ Front/SsokSsok/src/App.jsx
@@ -1,18 +1,8 @@
-export default function App() {
-  return (
-    <div className="flex justify-center items-center h-screen">
-      <h1 className="text-3xl font-bold">메인 회원가입 + 로그인 페이지</h1>
-      <h1 className="font-whitechalk text-4xl" style={{ fontFamily: "Whitechalk" }}>
-        하얀분필 폰트입니다.
-      </h1>
-      <h1 className="font-ganpan text-4xl" style={{ fontFamily: "KccGanpan" }}>
-        KCC 간판 폰트입니다.
-      </h1>
-      <h1 className="font-dodam text-4xl" style={{ fontFamily: "KccDodam" }}>
-        KCC 도담도담 폰트입니다.
-      </h1>
-    </div>
+import React from "react";
+import SingupLoginMainPage from "./pages/main/signup_login_mainPage"
 
-  );
+
+export default function App() {
+  return <SingupLoginMainPage/>;
 }
 
