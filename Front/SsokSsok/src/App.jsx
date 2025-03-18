f68fcd439203b11517b6f66bf2ac29e10a8bffd1
unknown <winlouder3@gmail.com>
2025-03-18 16:50:27 +0900

    S12P21E201-59 [Infra] test: cicd test

--- Front/SsokSsok/src/App.jsx
+++ Front/SsokSsok/src/App.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import SignupLoginMainPage from "./pages/auth/SignupLoginMainPage"
 import SignupPage from "./pages/auth/SignupPage"
-import LoginPage from "./pages/auth/loginPage"
+import LoginPage from "./pages/auth/LoginPage"
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom"
 import MainPage from "./pages/main/MainPage";
 
