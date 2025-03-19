8d08b9884afcb4ff87f966d7e3ac0d0a85bff2a6
ddoriboy <ddoriboy@naver.com>
2025-03-19 10:56:29 +0900

    S12P21E201-77 [BE] feat: 유저 이름, 닉네임 수정 API 개발

--- Back/user-service/src/main/java/com/mongle/userservice/service/UserService.java
+++ Back/user-service/src/main/java/com/mongle/userservice/service/UserService.java
@@ -1,5 +1,10 @@
 package com.mongle.userservice.service;
 
+import com.mongle.userservice.dto.request.UpdateNameRequestDTO;
+import com.mongle.userservice.dto.request.UpdateNickNameRequestDTO;
+
 public interface UserService {
     void deleteUser(String userPk);
+    void updateUserName(String userPk, UpdateNameRequestDTO request);
+    void updateUserNickName(String userPk, UpdateNickNameRequestDTO request);
 }
--- Back/user-service/src/main/java/com/mongle/userservice/service/UserServiceImpl.java
+++ Back/user-service/src/main/java/com/mongle/userservice/service/UserServiceImpl.java
@@ -1,6 +1,8 @@
 package com.mongle.userservice.service;
 
 
+import com.mongle.userservice.dto.request.UpdateNameRequestDTO;
+import com.mongle.userservice.dto.request.UpdateNickNameRequestDTO;
 import com.mongle.userservice.entity.User;
 import com.mongle.userservice.exception.CustomException;
 import com.mongle.userservice.exception.ErroCode;
@@ -27,5 +29,27 @@ public class UserServiceImpl implements UserService{
         // 2. 유저가 존재하면 DB에서 삭제
         userMapper.deleteUser(userId);
 
+    }
+
+    public void updateUserName(String userPk, UpdateNameRequestDTO request){
+        // 1. 빈칸을 입력할 경우 에러 처리
+        if (request.getNewName() == null || request.getNewName().trim().isEmpty()) {
+            throw new CustomException(ErroCode.INVALID_INPUT);
+        }
+
+        int userId = Integer.parseInt(userPk);
+
+        userMapper.updateUserName(userId, request.getNewName());
+    }
+
+    public void updateUserNickName(String userPk, UpdateNickNameRequestDTO request){
+        // 1. 빈칸을 입력할 경우 에러 처리
+        if (request.getNewNickName() == null || request.getNewNickName().trim().isEmpty()) {
+            throw new CustomException(ErroCode.INVALID_INPUT);
+        }
+
+        int userId = Integer.parseInt(userPk);
+
+        userMapper.updateUserNickName(userId, request.getNewNickName());
     }
 }
