12410b17f9d005a4bf9a4c8d8aad1dcc92e7ff18
unknown <winlouder3@gmail.com>
2025-03-20 17:21:54 +0900

    S12P21E201-81 [Infra] test: cicd test

--- Back/apigateway-service/src/main/java/com/mongle/apigatewayservice/CicdTest.java
+++ Back/apigateway-service/src/main/java/com/mongle/apigatewayservice/CicdTest.java
@@ -2,6 +2,6 @@ package com.mongle.apigatewayservice;
 
 public class CicdTest {
     public void test(){
-        System.out.println("test 123432233232");
+        System.out.println("test 123432233232dwdw");
     }
 }
