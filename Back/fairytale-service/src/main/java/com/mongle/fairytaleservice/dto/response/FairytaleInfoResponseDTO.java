fec627b704ca779a3cc783ff40f74bd08c402e3c
ddoriboy <ddoriboy@naver.com>
2025-03-24 08:36:51 +0900

    S12P21E201-100 [BE] 동화책 진행사항 정보 조회 기능 추가

--- Back/fairytale-service/src/main/java/com/mongle/fairytaleservice/dto/response/FairytaleInfoResponseDTO.java
+++ Back/fairytale-service/src/main/java/com/mongle/fairytaleservice/dto/response/FairytaleInfoResponseDTO.java
@@ -2,13 +2,12 @@ package com.mongle.fairytaleservice.dto.response;
 
 import lombok.*;
 
+import java.util.List;
+
 @Data
 public class FairytaleInfoResponseDTO {
-    private Integer fairytalePk;
-    private String title;
-    private Integer count;
-    private String coverImgUrl;
-    private String first;
-    private String second;
-    private String metadata;
+    private FairytaleInfoDTO fairytale;
+    private List<ProgressInfoDTO> progressList;
 }
+
+
--- Back/fairytale-service/src/main/java/com/mongle/fairytaleservice/service/FairytaleService.java
+++ Back/fairytale-service/src/main/java/com/mongle/fairytaleservice/service/FairytaleService.java
@@ -3,5 +3,5 @@ package com.mongle.fairytaleservice.service;
 import com.mongle.fairytaleservice.dto.response.FairytaleInfoResponseDTO;
 
 public interface FairytaleService {
-    FairytaleInfoResponseDTO findFairytaleById(Integer fairytalePk);
+    FairytaleInfoResponseDTO findFairytaleById(Integer fairytalePk, String userPk);
 }
