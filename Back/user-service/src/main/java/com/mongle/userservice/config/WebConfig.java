2e96b07197c18542795fd5e9220ad959873c56b6
ddoriboy <ddoriboy@naver.com>
2025-03-24 11:58:24 +0900

    S12P21E201-103 [BE] fix: CORS 허용 주소 추가

--- Back/user-service/src/main/java/com/mongle/userservice/config/WebConfig.java
+++ Back/user-service/src/main/java/com/mongle/userservice/config/WebConfig.java
@@ -15,7 +15,7 @@ public class WebConfig implements WebMvcConfigurer {
                         "http://127.0.0.1:5080",
                         "http://192.168.30.178:5080",
                         "https://j12e201.p.ssafy.io") // 배포된 프론트 주소
-                .allowedMethods("GET", "POST", "PUT", "DELETE","PATCH")
+                .allowedMethods("GET", "POST", "PUT", "DELETE","PATCH", "OPTIONS")
                 .allowedHeaders("*")
                 .exposedHeaders("Set-Cookie") // 쿠키를 클라이언트에서 읽을 수 있도록 허용
                 .allowCredentials(true); // 쿠키 허용
