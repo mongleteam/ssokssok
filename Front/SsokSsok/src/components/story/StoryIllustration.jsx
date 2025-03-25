a190ab2de4d31d68fa296e0e70116949b1098b14
xongeeuse <xongeeuse@gmail.com>
2025-03-25 09:06:58 +0900

    S12P21E201-62 [FE] feat: 삽화 테두리 tornpaper 효과 적용

---
+++ Front/SsokSsok/src/components/story/StoryIllustration.jsx
@@ -0,0 +1,41 @@
+import React, { useEffect } from 'react';
+import illustration from '../../assets/images/illustration_default.png';
+
+function StoryIllustration() {
+  useEffect(() => {
+    if (window.tornpaperLoaded) return; // 이미 로드된 경우 리턴
+    
+    const script = document.createElement('script');
+    script.src = '/assets/js/tornpaper.min.js';
+    script.onload = () => {
+      if (typeof Tornpaper !== 'undefined') {
+        new Tornpaper({
+          filterName: "filter_tornpaper",
+          seed: 1,
+          tornFrequency: 0.05,
+          tornScale: 10,
+          grungeFrequency: 0.03,
+          grungeScale: 3
+        });
+        window.tornpaperLoaded = true;
+      }
+    };
+    document.body.appendChild(script);
+
+    return () => {
+      document.body.removeChild(script);
+    };
+  }, []);
+
+  return (
+    <div className="relative w-[600px] h-auto torn-effect">
+      <img
+        src={illustration}
+        alt="Illustration"
+        style={{ filter: 'url(#filter_tornpaper)' }}
+      />
+    </div>
+  );
+}
+
+export default StoryIllustration;
--- Front/SsokSsok/src/pages/multi/MultiPage.jsx
+++ Front/SsokSsok/src/pages/multi/MultiPage.jsx
@@ -1,16 +1,18 @@
 import React from "react";
 import '../../styles/book_background.css'; // CSS 파일 임포트
+import StoryIllustration from '../../components/story/StoryIllustration'; // StoryIllustration 컴포넌트 임포트
 
 function MultiPage() {
-    return (
-        <div className="book-background-container">
-        {/* 멀티 모드 화면 콘텐츠 */}
-        <div className="content-container">
-            <h1 className="font-cafe24">멀티 모드</h1>
-            {/* 삽화, 미션, 힌트 등 하위 컴포넌트 */}
-        </div>
+  return (
+    <div className="book-background-container">
+      {/* 멀티 모드 화면 콘텐츠 */}
+      <div className="content-container">
+        <h1 className="font-cafe24">멀티 모드</h1>
+        {/* StoryIllustration 컴포넌트 출력 */}
+        <StoryIllustration />
       </div>
-    );
-  }
+    </div>
+  );
+}
 
-export default MultiPage
+export default MultiPage;
