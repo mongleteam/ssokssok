e6d9dd8bb96fa4326e79b0b7aae998f6cd53a569
skdud5126 <skdud5126@naver.com>
2025-03-25 11:43:11 +0900

    S12P21E201-95 [FE] feat : s3에서 압축 파일 해제 및 사용 가능(삽화, 사운드, 대사)

--- Front/SsokSsok/src/pages/single/SinglePage.jsx
+++ Front/SsokSsok/src/pages/single/SinglePage.jsx
@@ -2,15 +2,49 @@ import React, { useState, useEffect } from "react";
 import '../../styles/book_background.css'; // CSS 파일 임포트
 import StoryHeader from "../../components/StoryHeader";
 import PhotoModal from "../../components/story/PhotoModal";
+import SingleStoryRenderer from "../../components/story/SingleStoryRenderer";
+import JSZip from "jszip";
 
 function SinglePage() {
     const [ showModal, setShowModal ] = useState(true); // 진입 시 자동 오픈픈
+    const [story, setStory] = useState(null); // ✅ 추가
+    const [assets, setAssets] = useState(null); // ✅ 추가
+    
 
     useEffect(() => {
-        const timer = setTimeout(() => setShowModal(false), 10000);
-        return () => clearTimeout(timer);
-    }, []);
+        // 진입하자마자 preload 시작!
+        const preload = async () => {
+          console.log("📦 ZIP 다운로드 시작");
+          const zipUrl = "https://ssafy-mongle.s3.ap-southeast-2.amazonaws.com/HanselAndGretelData_single.zip";
+          const res = await fetch(zipUrl);
+          const blob = await res.blob();
+          console.log("📥 ZIP 다운로드 완료. 해제 중...");
+          const zip = await JSZip.loadAsync(blob);
 
+          console.log("🔓 ZIP 해제 완료");
+    
+          const fileMap = {};
+          const fileNames = Object.keys(zip.files);
+          console.log("📁 포함된 파일 목록:", fileNames);
+    
+          for (const fileName of fileNames) {
+            const file = zip.file(fileName);
+            if (file) {
+              const contentBlob = await file.async("blob");
+              fileMap[fileName] = URL.createObjectURL(contentBlob);
+            }
+          }
+    
+          const storyFile = zip.file("story_single.json");
+          const storyText = await storyFile.async("string");
+          const storyJson = JSON.parse(storyText);
+    
+          setAssets(fileMap);
+          setStory(storyJson);
+        };
+    
+        preload();
+      }, []);
     return (
         <div className="book-background-container">
         {/* 멀티 모드 화면 콘텐츠 */}
@@ -18,6 +52,10 @@ function SinglePage() {
 
         {/* 포토모달 */}
         <PhotoModal isOpen={showModal} onClose={() => setShowModal(false)} />
+
+        {!showModal && story && assets && (
+        <SingleStoryRenderer story={story} assets={assets} />
+      )}
         
 
         <div className="content-container">
