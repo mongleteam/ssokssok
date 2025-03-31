bd0d3f5ff229602d295b08c3219a83703738ebcc
xongeeuse <xongeeuse@gmail.com>
2025-03-31 17:30:28 +0900

    [FE] feat: 진행상황 업데이트 api 추가 작성

--- Front/SsokSsok/src/apis/multiApi.js
+++ Front/SsokSsok/src/apis/multiApi.js
@@ -7,3 +7,7 @@ export const sendInviteApi = (friendId) => {
 export const createProgressApi = async (data) => {
   return await authApi.post("/fairytale/create", data);
 };
+
+export const updateProgressApi = (progressPk, body) => {
+  return authApi.patch(`/fairytale/${progressPk}`, body);
+};
