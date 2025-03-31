dfdcd6666eddee04d6ac2b6076c792961e5305c1
unknown <winlouder3@gmail.com>
2025-03-31 10:34:29 +0900

    S12P21E201-111 [BE] docs: Socket 연결

--- Back/socket-service/src/main/java/com/mongle/socketservice/socket/SocketEventHandler.java
+++ Back/socket-service/src/main/java/com/mongle/socketservice/socket/SocketEventHandler.java
@@ -11,7 +11,7 @@ import jakarta.annotation.PostConstruct;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Component;
-// cicd test12
+// cicd test123
 @Component
 @RequiredArgsConstructor
 @Slf4j
