924d4e29670957ccb1b6e602a8b593087b205d22
skdud5126 <skdud5126@naver.com>
2025-04-02 11:25:47 +0900

    S12P21E201-171 [FE] feat : 싱글모드 미디어파이프(열쇠얻기) 기능 구현 완료

--- Front/SsokSsok/src/components/missions/index.js
+++ Front/SsokSsok/src/components/missions/index.js
@@ -3,6 +3,7 @@ import WebcamCollectStoneMission from "../missions/WebcamCollectStoneMission";
 import WebcamReadTextMission from "../missions/WebcamReadTextMission";
 import HandHoldBreadMission from "../missions/HandHoldBreadMission";
 import EatCookieMission from "../missions/EatCookieMisson";
+import WebcamGetKeyMission from "../missions/WebcamGetKeyMission";
 import RockScissorsPaperMission from "../missions/RockScissorsPaperMission";
 import WebcamCleanMission from "./WebcamCleanMission";
 import TreasureHuntMission from "./TreasureHuntMission";
@@ -14,6 +15,7 @@ export const missionMap = {
   "webcam-readtext": WebcamReadTextMission, // STT 미션션
   "hand-hold-bread": HandHoldBreadMission, // 빵찾기 미션
   "webcam-eatcookie": EatCookieMission, // 쿠키 미션
+  "webcam-getkey": WebcamGetKeyMission, // 열쇠얻기 미션션
   "webcam-get-magicbook": RockScissorsPaperMission, // 가위바위보 미션
   "webcam-clean": WebcamCleanMission, // 청소 미션
   "treasure-hunt": TreasureHuntMission, // 보물찾기 미션
