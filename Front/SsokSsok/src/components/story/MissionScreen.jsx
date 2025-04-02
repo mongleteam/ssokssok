f551c3a2c04230c3297b122c73fbd90e59bedd3b
Yujiin <jiin4083@naver.com>
2025-04-02 17:50:47 +0900

    -ing

--- Front/SsokSsok/src/components/story/MissionScreen.jsx
+++ Front/SsokSsok/src/components/story/MissionScreen.jsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect  } from "react";
+import React, { useState, useEffect } from "react";
 import boardImage from "../../assets/images/board2.png";
 import hintIcon from "../../assets/images/hint_icon.png"; // 힌트 아이콘 경로
 import HintModal from "../../components/story/HintModal"; // 힌트 모달 컴포넌트 임포트
@@ -9,19 +9,19 @@ const MissionScreen = ({ storyData, assets }) => {
 
   useEffect(() => {
     const fetchMission = async () => {
-      if (!storyData?.instructions) {
+      if (!storyData?.mission?.instructions) {
         setMissionText("지시사항이 없습니다.");
         return;
       }
 
       try {
-        const missionUrl = assets[storyData.instructions];
+        const missionUrl = assets[storyData.mission.instructions];
         if (!missionUrl) {
           throw new Error("지시사항 파일 URL이 없습니다.");
         }
 
         const res = await fetch(missionUrl);
-        if (!res.ok) { 
+        if (!res.ok) {
           throw new Error(`HTTP error! status: ${res.status}`);
         }
         const text = await res.text();
@@ -47,11 +47,13 @@ const MissionScreen = ({ storyData, assets }) => {
       {/* 미션 텍스트 */}
       <div className="text-center font-cafe24">
         <h2 className="text-3xl font-bold pb-2">미션!</h2>
-        <p className="mt-2 text-lg font-medium whitespace-pre-line">{missionText}</p>
+        <p className="mt-2 text-lg font-medium whitespace-pre-line">
+          {missionText}
+        </p>
       </div>
 
       {/* 힌트 아이콘 */}
-      {storyData.hintImage && (
+      {storyData.mission?.hintImage && (
         <button
           className="absolute top-1 right-2 w-12 h-12 flex items-center justify-center bg-transparent hover:scale-110 transition-transform duration-300 m-6 z-10"
           onClick={handleHintClick}
@@ -64,7 +66,7 @@ const MissionScreen = ({ storyData, assets }) => {
       {isHintModalOpen && (
         <HintModal
           onClose={() => setIsHintModalOpen(false)}
-          hintImage={storyData.hintImage}
+          hintImage={storyData.mission.hintImageFile}
         />
       )}
     </div>
