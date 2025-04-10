599defabb07c0e38b6834c5a890c7c8a20e36aad
jh.nam0324 <jh.nam0324@gmail.com>
2025-04-10 14:15:19 +0900

    [BE] fix: 알림 소켓 연결 수정

--- Back/friend-service/src/main/java/com/mongle/friendservice/client/SocketServiceClient.java
+++ Back/friend-service/src/main/java/com/mongle/friendservice/client/SocketServiceClient.java
@@ -2,12 +2,13 @@ package com.mongle.friendservice.client;
 
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 
 @FeignClient(name = "socket-service")
 public interface SocketServiceClient {
 
     @PostMapping("/api/socket/disconnect")
-    public void disconnect(String roomId);
+    void disconnect(@RequestParam("roomId") String roomId);
 
 }
